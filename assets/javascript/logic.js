9a8705d5cae4168121ebc686a2a9452095452215
Desmond Mullen <dsm@busylittlestudios.com>
2019-02-03 02:13:44 -0500

    working

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -11,12 +11,9 @@ $(document).ready(function () {
 
     var database = firebase.database();
     var userID;
-    var userSignedIn;
     var userName;
-    var userIdentificationPath;
-    var userInstancesPath;
-    var userMessagesPath;
-    var userChoicePath;
+    var messagesPath;
+    var choicePath;
     var theChoice;
     var playerNumberOneOrTwo;
     var otherPlayerMapShown = false;
@@ -46,7 +43,6 @@ $(document).ready(function () {
     function doAddEntry(automatic) {
         let todaysDate = new Date().toLocaleDateString("en-US");
         let currentTime = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-        console.log("do add entry:" + automatic + ", userID is: " + userID);
         if (automatic != "connected" && automatic != "disconnected") {
             var entryMessage = $("#input-message").val().trim() + "<br>";
         } else {
@@ -56,7 +52,7 @@ $(document).ready(function () {
                 var entryMessage = "[disconnected]<br>";
             };
         };
-        database.ref(userMessagesPath).set({
+        database.ref("messages").set({
             dateTime: todaysDate + " " + currentTime,
             userName: userName,
             message: entryMessage,
@@ -68,13 +64,13 @@ $(document).ready(function () {
         $("#input-message").val("");
     };
 
-    database.ref(userMessagesPath).on("value", function (snapshot) {
-        let theMessageDateTime = snapshot.child(userMessagesPath + "/dateTime/").val();
-        let theMessageUserName = snapshot.child(userMessagesPath + "/userName/").val();
-        let theMessageMessage = snapshot.child(userMessagesPath + "/message/").val();
-        let theCurrentLat = parseFloat(snapshot.child(userMessagesPath + "/currentLat/").val());
-        let theCurrentLong = parseFloat(snapshot.child(userMessagesPath + "/currentLong/").val());
-        let theCurrentGeolocation = snapshot.child(userMessagesPath + "/currentGeolocation/").val();
+    database.ref(messagesPath).on("value", function (snapshot) {
+        let theMessageDateTime = snapshot.child(messagesPath + "/dateTime/").val();
+        let theMessageUserName = snapshot.child(messagesPath + "/userName/").val();
+        let theMessageMessage = snapshot.child(messagesPath + "/message/").val();
+        let theCurrentLat = parseFloat(snapshot.child(messagesPath + "/currentLat/").val());
+        let theCurrentLong = parseFloat(snapshot.child(messagesPath + "/currentLong/").val());
+        let theCurrentGeolocation = snapshot.child(messagesPath + "/currentGeolocation/").val();
         if (theMessageDateTime != null && theMessageDateTime + theMessageMessage != theLastMessage) {
             $("#message-display").prepend("<span class='monospace'>" + theMessageDateTime + " <strong>" + theMessageUserName + "</strong>:</span> " + theMessageMessage);
             theLastMessage = theMessageDateTime + theMessageMessage;
@@ -95,11 +91,11 @@ $(document).ready(function () {
         console.log("entries-error: " + errorObject.code);
     });
 
-    database.ref(userChoicePath).on("value", function (snapshot) {
-        let playerOneChoice = (snapshot.child(userChoicePath + "/playerOneChoice/").val());
-        let playerTwoChoice = (snapshot.child(userChoicePath + "/playerTwoChoice/").val());
+    database.ref(choicePath).on("value", function (snapshot) {
+        let playerOneChoice = (snapshot.child(choicePath + "/playerOneChoice/").val());
+        let playerTwoChoice = (snapshot.child(choicePath + "/playerTwoChoice/").val());
         if (playerOneChoice != null && playerTwoChoice != null) {
-            database.ref(userChoicePath).set({
+            database.ref(choicePath).set({
                 playerOneChoice: null,
                 playerTwoChoice: null,
             });
@@ -114,12 +110,9 @@ $(document).ready(function () {
         $("#input-message").val("");
         $("#message-display").text("");
         userID = "";
-        userSignedIn = "";
         userName = "";
-        userIdentificationPath = "";
-        userInstancesPath = "";
-        userMessagesPath = "";
-        userChoicePath = "";
+        messagesPath = "";
+        choicePath = "";
         userLatitude;
         userLongitude;
         userLatLong;
@@ -131,6 +124,7 @@ $(document).ready(function () {
 
     connectedRef.on("value", function (connectedSnapshot) {
         if (connectedSnapshot.val()) {
+            console.log("requesting connection, number online: " + theNumberOnline);
             var theConnection = connectionsRef.push(true);
             theConnection.onDisconnect().remove();
         };
@@ -170,23 +164,6 @@ $(document).ready(function () {
         console.log("anonymous login error: " + errorCode, errorMessage);
         // ...
     });
-
-    function turnURLIntoUserInstancesPath(theLink) {
-        console.log("turn URL into user instances path");
-        if (theLink == null || path == "" || path == undefined) {
-            theLink = window.location.href;
-        }
-        window.localStorage.setItem("theLastURLParameters", theLink);
-        window.history.replaceState({}, document.title, window.location.href.split('?')[0]);//cleans up sign-in link params
-        let theInstancesPath = (theLink.substring((theLink.indexOf("?") + 1), theLink.indexOf("&")));
-        if (theInstancesPath != null) {
-            userInstancesPath = decodeURIComponent(theInstancesPath);
-            userMessagesPath = userInstancesPath + "/messages";
-            console.log("new path: " + decodeURIComponent(theInstancesPath));
-        } else {
-            console.log("new path was null, existing path is: " + userInstancesPath);
-        };
-    };
 
     function signOut() {
         if (playerNumberOneOrTwo === 1) {
@@ -232,11 +209,8 @@ $(document).ready(function () {
     //#endregion
 
     function initializeDatabaseReferences() {
-        let localStorageUIPath = window.localStorage.getItem("userInstancesPath");
-        let localStorageLastURLParams = window.localStorage.getItem("theLastURLParameters");
         firebase.auth().onAuthStateChanged(function (user) {
             if (user) {
-                console.log("auth state changed: " + user.uid);
                 userID = user.uid; //when connecting by link, this will be the same user
                 let shortUserID = Math.floor(Math.random() * 1000 + 1000);
                 userName = prompt("Please enter a name to use for sending messages. If you don't choose one, we'll call you by this random number:", shortUserID);
@@ -244,26 +218,8 @@ $(document).ready(function () {
                     userName = shortUserID;
                 };
                 // User is signed in.
-                userSignedIn = true;
-                userIdentificationPath = "users/" + userID + "/identification";
-                if (window.location.href.indexOf("?") > 0) {
-                    turnURLIntoUserInstancesPath();
-                    console.log("user ID after signout: " + userID);
-                } else {
-                    if (localStorageUIPath != null) {
-                        console.log("restoring userInstancePath from local storage");
-                        userInstancesPath = localStorageUIPath;
-                    } else {
-                        userInstancesPath = "users/" + userID + "/instances/" + (+new Date());
-                    }
-                    userMessagesPath = userInstancesPath + "/messages";
-                }
-                userChoicePath = userInstancesPath + "/choice";
-                playersPath = userInstancesPath + "/players";
-
-                if (localStorageLastURLParams != null) {
-                    turnURLIntoUserInstancesPath(localStorageLastURLParams);
-                };
+                choicePath = "choice";
+                playersPath = "players";
                 getLocation();
                 setTimeout(function () {
                     doAddEntry("connected");
@@ -296,14 +252,12 @@ $(document).ready(function () {
         userLatitude = parseFloat(position.coords.latitude);
         userLongitude = parseFloat(position.coords.longitude);
         if (initMapLatLong != userLatitude, userLongitude) {
-            console.log("redoing initMap: " + initMapLatLong + " / " + userLatitude, userLongitude);
             initMap();
         }
     }
 
     function initMap() {
         setTimeout(function () {
-            console.log("init map: " + userLatitude, userLongitude);
             initMapLatLong = userLatitude, userLongitude;
             var userLatLong = { lat: userLatitude, lng: userLongitude };
             map = new google.maps.Map(document.getElementById("map-left"), {
@@ -355,11 +309,11 @@ $(document).ready(function () {
     $("#commit").click(function (event) {
         console.log("commit: " + theChoice + " as player " + playerNumberOneOrTwo);
         if (playerNumberOneOrTwo === 1) {
-            database.ref(userChoicePath).update({
+            database.ref(choicePath).update({
                 playerOneChoice: theChoice,
             });
         } else {
-            database.ref(userChoicePath).update({
+            database.ref(choicePath).update({
                 playerTwoChoice: theChoice,
             });
         };
@@ -373,5 +327,5 @@ $(document).ready(function () {
         }, 500);
     };
 
-    console.log("v1.32");
+    console.log("v1.33");
 });
